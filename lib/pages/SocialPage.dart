87124b63064076c0f23900097aa73ca310d2e0e5
kambhani <63814718+kambhani@users.noreply.github.com>
2023-01-21 11:39:50 -0500

    Fix SocialPage.dart

--- lib/pages/SocialPage.dart
+++ lib/pages/SocialPage.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: _title,
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+      ),
       home: MyStatefulWidget(),
     );
   }
@@ -28,23 +32,23 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   List<int> top = <int>[];
   List<int> bottom = <int>[];
-  List<String> goals = [
-    "Run a mile",
-    "eat sushi for the first time",
-    "take a nap",
-    "Go fishing",
-    "Eat pie",
-    "Finish math homework",
-    "Take a walk",
-    "Go to Starbucks"
+
+  List<List<String>> goals = [
+    ["Chris", "Run a mile", "Eat sushi", "Take a nap"],
+    ["Joey", "Go fishing", "Eat a pie", "Finish math homework"],
+    ["Anish", "Finish testing code", "Play Clash Royale", "Go home"],
+    ["Karthik", "Get flutter to work on Mac", "Win a 5-leg parlay", "Watch NBA games"]
   ];
+
   @override
   Widget build(BuildContext context) {
     const Key centerKey = ValueKey<String>('bottom-sliver-list');
+    var theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Social Page'),
-        backgroundColor: Colors.deepOrange[500],
+        backgroundColor: theme.colorScheme.primary,
         leading: IconButton(
           icon: const Icon(Icons.add),
           onPressed: () {
@@ -58,32 +62,27 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           },
         ),
       ),
-      body: CustomScrollView(
-        center: centerKey,
-        slivers: <Widget>[
-          SliverList(
-            key: centerKey,
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: index % 2 == 0 ? Colors.blue : Colors.yellow,
-                  height: 100,
-                  child: Column(
-                    children: <Widget>[
-                      Text('Goal: ${goals[index]}',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      Text('Owner: Chris'),
-                    ],
+      body: Padding (
+        padding: EdgeInsets.all(0),
+        child: ListView.builder(
+            itemCount: goals.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding (
+                padding: EdgeInsets.all(10),
+                child: ListTile(
+                  leading: const Icon(
+                      Icons.account_circle,
+                    size: 32,
                   ),
-                );
-              },
-              childCount: bottom.length,
-            ),
-          ),
-        ],
-      ),
+                  tileColor: index % 2 == 0 ? theme.colorScheme.inversePrimary : theme.colorScheme.primaryContainer,
+                  title: Text('${goals[index][0]}'),
+                  subtitle: Text("${goals[index][1]}\n${goals[index][2]}\n${goals[index][3]}"),
+                  isThreeLine: true,
+                ),
+              );
+            }
+        ),
+      )
     );
   }
 }
