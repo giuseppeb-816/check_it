6705126ce697322512c049a39062aed76003e473
kambhani <63814718+kambhani@users.noreply.github.com>
2023-01-20 22:41:35 -0500

    Update Login or Sign Up Page

--- lib/LoginOrSignUpPage.dart
+++ lib/LoginOrSignUpPage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginOrSignUp extends StatelessWidget {
+class LoginOrSignUpPage extends StatelessWidget {
+  const LoginOrSignUpPage({super.key});
+
+  @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -9,7 +17,10 @@ class LoginOrSignUp extends StatelessWidget {
           Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text("CheckIt"),
+              Text(
+                "CheckIt",
+                style: style,
+              ),
               const SizedBox(height: 100),
               ElevatedButton(
                 onPressed: () {
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
       ),
-      home: LoginOrSignUp(),
+      home: LoginOrSignUpPage(),
     );
   }
 }
